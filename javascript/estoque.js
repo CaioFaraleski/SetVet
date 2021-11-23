40c89b8f1f341c50ebc7207ef0646e4b7bc55c76
Caio Tadeu <caiotadeu_2012@hotmail.com>
2021-11-23 11:55:22 -0300

    Pagina produtos finalizada

--- javascript/estoque.js
+++ javascript/estoque.js
@@ -1,15 +1,44 @@
 let orderItem = document.querySelector(".order-list");
 let arrowOrderBy = document.querySelector(".orderby");
 let addProduct = document.querySelector(".left-infos");
-let exit = document.querySelector(".exit")
-let wind = document.querySelector(".hidden"); 
+let exit = document.querySelector(".exit");
+let exitEdit = document.querySelector(".exit-edits");
+let wind = document.querySelector(".hidden");
+let windEdit = document.querySelector(".hidden-edits");
+let edits = document.querySelectorAll(".item");
 let names = false;
 let cod = false;
 let quant = false;
 let value = false;
 
+
+edits.forEach(function (item) {
+    item.children[5].children[0].addEventListener("click", function(event) {
+        let pai = event.target.parentNode.parentNode;
+        let items = [];
+        for (let i = 0; i < 5; i++) {
+            items[i] = `${pai.children[i].innerText}`;  
+        }
+        windEdit.style.display = "block";
+        wind.style.display = "none";
+        windEdit.querySelector("#cod").value = `${items[0]}`;
+        windEdit.querySelector("#name").value = `${items[1]}`;
+        windEdit.querySelector("#date").value = `${items[2]}`;
+        windEdit.querySelector("#val").value = `${items[3]}`;
+        windEdit.querySelector("#quant").value = `${items[4]}`;
+        
+    
+        // editProduct.style.display = "block";
+    });
+});
+
+exitEdit.children[0].addEventListener("click", function() {
+    windEdit.style.display = "none";
+});
+
 addProduct.addEventListener("click", function() {
     wind.style.display = "block";
+    windEdit.style.display = "none";
 });
 
 exit.children[0].addEventListener("click", function() {

46385264e80b8ef03aac395a2d9f993942a3f9a9
Caio Tadeu <caiotadeu_2012@hotmail.com>
2021-11-25 12:13:03 -0300

    Funcao para ano menor que 2021

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -7,12 +7,49 @@ let monthPhp = month[11].innerText;
 let yearPhp = document.querySelector(".year").innerText;
 let dayPhp = document.querySelector(".theday").querySelector("span").innerText;
 let hiddenInput = document.querySelectorAll(".input-hidden");
+let monthCore = "Dezembro";
+let numMonthCore = 12;
+let yearCore = "2021";
+let squareCore = 3;
+let squareCoreFinal = 8;
+
+function setYear () {
+    if(localStorage.year !== undefined) {
+        document.querySelector(".year").innerText = localStorage.year;
+    }
+    else {
+        localStorage.setItem("year", Number(yearCore));
+    }
+}
+
+function setDay () {
+    if(localStorage.day !== undefined) {
+        document.querySelector(".theday").querySelector("span").innerText = localStorage.day;
+    }
+    else {
+        localStorage.setItem("day", "1");
+    }
+}
+
+function setMonth () {
+    if(localStorage.month !== undefined) {
+        document.querySelector(".month").innerText = localStorage.month;
+    }
+    else {
+        localStorage.setItem("month", monthCore);
+    }
+}    
+setYear();
+setMonth();
+setDay();
 
 leftArrow.addEventListener("click", function () {
     let year = document.querySelector(".year");
     let yearNumber = Number(year.innerText) - 1;
     year.innerText = yearNumber;
     yearPhp = yearNumber;
+    localStorage.setItem("year", `${yearPhp}`);
+    console.log(squareAlign(localStorage.month, Number(localStorage.year)));
 });
 
 rightArrow.addEventListener("click", function () {
@@ -20,6 +57,7 @@ rightArrow.addEventListener("click", function () {
     let yearNumber = Number(year.innerText) + 1;
     year.innerText = yearNumber;
     yearPhp = yearNumber;
+    localStorage.setItem("year", `${yearPhp}`);
 });
 
 month.forEach(function (theMonth) {
@@ -27,6 +65,7 @@ month.forEach(function (theMonth) {
         let named = event.target.innerText;
         document.querySelector(".month").innerText = `${named}`;
         monthPhp = named;
+        localStorage.setItem("month", `${named}`);
     });
 });
 
@@ -41,8 +80,9 @@ days.forEach(function (theDay) {
             let InputTwo = hiddenInput[1];
             document.querySelector(".theday").querySelector("span").innerText = `${named}`;
             dayPhp = named;
+            localStorage.setItem("day", `${named}`);
             inputOne.value = yearPhp;
-            InputTwo = monthPhp;
+            InputTwo.value = monthPhp;
         }
     });
 });
@@ -66,7 +106,101 @@ function Bissexto (year) {
     }
 }
 
-function daysOfMonth (mon) {
+function numDays (num, yr) {
+    if(Math.sign(num) === 1) {
+        if(num % 12 === 1 ) {
+            return 31;
+        }
+        if(num % 12 === 2) {
+            if(Bissexto(yr)) {
+                return 29;
+            }
+            else {
+                return 28;
+            }
+        }
+        if(num % 12 === 3) {
+            return 31;
+        }
+        if(num % 12 === 4) {
+            return 30;
+        }
+        if(num % 12 === 5) {
+            return 31;
+        }
+        if(num % 12 === 6) {
+            return 30;
+        }
+        if(num % 12 === 7) {
+            return 31;
+        }
+        if(num % 12 === 8) {
+            return 31;
+        }
+        if(num % 12 === 9) {
+            return 30;
+        }
+        if(num % 12 === 10) {
+            return 31;
+        }
+        if(num % 12 === 11) {
+            return 30;
+        }
+        if(num % 12 === 0) {
+            return 31;
+        }
+    }
+    else if (Math.sign(num) === -1) {
+        if(num % -12 === 0) {
+            return 31;
+        }
+        if(num % -12 === -11) {
+            if(Bissexto(yr)) {
+                return 29;
+
+            }
+            else {
+                return 28;
+
+            }
+        }
+        if(num % -12 === -10) {
+            return 31;
+        }
+        if(num % -12 === -9) {
+            return 30;
+        }
+        if(num % -12 === -8) {
+            return 31;
+        }
+        if(num % -12 === -7) {
+            return 30;
+        }
+        if(num % -12 === -6) {
+            return 31;
+        }
+        if(num % -12 === -5) {
+            return 31;
+        }
+        if(num % -12 === -4) {
+            return 30;
+        }
+        if(num % -12 === -3) {
+            return 31;
+        }
+        if(num % -12 === -2) {
+            return 30;
+        }
+        if(num % -12 === -1) {
+            return 31;
+        }
+    }
+    else if(num === 0) {
+        return
+    }
+}
+
+function daysOfMonth (mon, yr) {
     let quant;
     let num;
     if(mon === "Janeiro") {
@@ -74,7 +208,7 @@ function daysOfMonth (mon) {
         num = 1;
     }
     if(mon === "Fevereiro") {
-        if(Bissexto(Number(document.querySelector(".year").innerText))) {
+        if(Bissexto(yr)) {
             quant = 29;
             num = 2;
         }
@@ -124,4 +258,63 @@ function daysOfMonth (mon) {
         num = 12;
     }
     return [quant, num];
+}
+
+function squareAlign (mon, year) {
+    let numOfDays = daysOfMonth(mon, year)[0];
+    let numOfMonth = daysOfMonth(mon, year)[1];
+    let numMonthSet = 11;
+    let numYearSet = 2021;
+    
+    if(numOfMonth === numMonthCore && year === numYearSet) {
+        return [squareCore, squareCoreFinal];
+    }
+
+    year = yearCore - Number(year);
+    year = year * 12;
+    console.log(year)
+
+    if(numOfMonth < numMonthCore || numOfMonth === numMonthCore) {
+        let squareCoreMutant = squareCore;
+        let squareFunction;
+        let squareFunctionFinal;
+        for (let i = 0; i < ((numMonthCore - daysOfMonth(mon, year)[1]) + year); i++) {
+            squareCoreMutant = 7 - squareCoreMutant;
+            numOfDays = numDays(numMonthSet, numYearSet);
+            if (42 - (squareCoreMutant + numOfDays) > 7) {
+                squareFunctionFinal = 7;
+                squareFunctionFinal += squareCoreMutant;
+                squareFunction = 42 - (squareFunctionFinal + numOfDays);
+                squareCoreMutant = squareFunction;
+                numMonthSet = numMonthSet - 1;
+                if(numMonthSet < 1) {
+                    numMonthSet = numMonthSet + 12;
+                    numYearSet = numYearSet - 1;
+                }
+            }
+            else if (42 - (squareCoreMutant + numOfDays) === 7) {
+                squareFunction = 0;
+                squareCoreMutant = 0;
+                squareFunctionFinal = 42 - numOfDays;
+                numMonthSet = numMonthSet - 1;
+                if(numMonthSet < 1) {
+                    numMonthSet = numMonthSet + 12;
+                    numYearSet = numYearSet - 1;
+                }
+            }
+            else {
+                squareFunctionFinal = squareCoreMutant;
+                squareFunction = 42 - (squareFunctionFinal + numOfDays);
+                squareCoreMutant = squareFunction;
+                numMonthSet = numMonthSet - 1;
+                if(numMonthSet < 1) {
+                    numMonthSet = numMonthSet + 12;
+                    numYearSet = numYearSet - 1;
+                }
+            }
+            
+        }
+        return [squareFunction, squareFunctionFinal];
+    }
+    
 }

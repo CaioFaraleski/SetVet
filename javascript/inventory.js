133967747ccfd7759f754d72b0c4583cc1166547
Caio Tadeu <caiotadeu_2012@hotmail.com>
2021-11-26 23:18:55 -0300

    mais mudancas no produtos

--- javascript/inventory.js
+++ javascript/inventory.js
@@ -3,14 +3,53 @@ let arrowOrderBy = document.querySelector(".orderby");
 let addProduct = document.querySelector(".left-infos");
 let exit = document.querySelector(".exit");
 let exitEdit = document.querySelector(".exit-edits");
+let exitMore = document.querySelector(".exit-more")
 let wind = document.querySelector(".hidden");
 let windEdit = document.querySelector(".hidden-edits");
+let windMore = document.querySelector(".hidden-moreorless");
 let edits = document.querySelectorAll(".item");
+let label = document.querySelector(".switch");
 let names = false;
 let cod = false;
 let quant = false;
 let value = false;
 
+function filterSpan (span) {
+    span.slice(3,9);
+}
+
+label.addEventListener("click", function() {
+    if(label.children[0].checked) {
+        label.parentNode.children[0].innerText = "Adicionar";
+        label.parentNode.children[0].style.color = "green";
+    }
+    else {
+        label.parentNode.children[0].innerText = "Remover";
+        label.parentNode.children[0].style.color = "red";
+    }
+});
+
+
+edits.forEach(function (item) {
+    item.children[6].children[0].addEventListener("click", function(event) {
+        let pai = event.target.parentNode.parentNode;
+        let items = [];
+        for (let i = 0; i < 5; i++) {
+            items[i] = `${pai.children[i].innerText}`;  
+        }
+        windMore.style.display = "none";
+        wind.style.display = "none";
+        windEdit.style.display = "block";
+        arrowOrderBy.children[1].style.display = "none";
+        arrowOrderBy.children[0].children[1].children[0].src = "https://caiofaraleski.github.io/SetVet/assets/img/inventory/down-arrow.png";
+        windEdit.querySelector("#cod").value = `${items[0]}`;
+        windEdit.querySelector("#name").value = `${items[1]}`;
+        windEdit.querySelector("#val").value = `${span(items[3])}`;
+        windEdit.querySelector("#quant").value = `${items[4]}`;
+        windEdit.querySelector("#cod").style.display = "none";
+        windEdit.querySelector(".cod").style.display = "none";
+    });
+});
 
 edits.forEach(function (item) {
     item.children[5].children[0].addEventListener("click", function(event) {
@@ -19,17 +58,17 @@ edits.forEach(function (item) {
         for (let i = 0; i < 5; i++) {
             items[i] = `${pai.children[i].innerText}`;  
         }
-        windEdit.style.display = "block";
+        windEdit.style.display = "none";
         wind.style.display = "none";
+        windMore.style.display = "block";
         arrowOrderBy.children[1].style.display = "none";
         arrowOrderBy.children[0].children[1].children[0].src = "https://caiofaraleski.github.io/SetVet/assets/img/inventory/down-arrow.png";
-        windEdit.querySelector("#cod").value = `${items[0]}`;
-        windEdit.querySelector("#name").value = `${items[1]}`;
-        windEdit.querySelector("#val").value = `${items[3]}`;
-        windEdit.querySelector("#quant").value = `${items[4]}`;
-        windEdit.querySelector("#cod").style.display = "none";
-        windEdit.querySelector(".cod").style.display = "none";
+        windMore.querySelector(".hidden-cod").value = `${items[0]}`;
     });
+});
+
+exitMore.children[0].addEventListener("click", function() {
+    windMore.style.display = "none";
 });
 
 exitEdit.children[0].addEventListener("click", function() {

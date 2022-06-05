fc18aea58e1868d42f0d29e537daf79eab6b7496
Caio Faraleski <caiotadeu_2012@hotmail.com>
2022-06-05 20:05:00 -0300

    finished responsiveness

--- javascript/register.js
+++ javascript/register.js
@@ -1,13 +1,13 @@
 funcButton.addEventListener('click', function() {
     if (funcButton.innerText === "Não possui uma conta?") {
         funcButton.innerText = "Já possui uma conta?";
-        loginOrRegister.children[0].children[0].innerText = "Crie uma conta";
+        loginOrRegister.children[0].children[0].children[0].innerText = "Registre-se";
         loginForm.style.display = "none";
         registerForm.style.display = "flex";
     }
     else {
         funcButton.innerText = "Não possui uma conta?";
-        loginOrRegister.children[0].children[0].innerText = "Entre";
+        loginOrRegister.children[0].children[0].children[0].innerText = "Entre";
         registerForm.style.display = "none";
         loginForm.style.display = "flex";
     }
